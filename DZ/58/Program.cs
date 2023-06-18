bc813f96c65925535cffb83efc108c0e589d7829
Sasha Dyachkov <sanya7765045@gmail.com>
2023-06-18 18:40:40 +0300

    Домашка

---
+++ DZ/58/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using static System.Console;
+
+// Функция для заполнения двумерного массива случайными числами в диапазоне от 0 до 10
+int[,] GetArray(int rows, int columns)
+{
+    int[,] array = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(0, 11);
+            Write($"{array[i, j]} ");
+        }
+        WriteLine();
+    }
+
+    return array;
+}
+
+// Функция для умножения двух матриц
+int[,] MatrMultiply(int[,] array, int[,] array2)
+{
+    // Получаем размеры матриц
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+    int rows2 = array2.GetLength(0);
+    int columns2 = array2.GetLength(1);
+
+    // Проверяем, можно ли умножить матрицы
+    if (columns != rows2)
+    {
+        WriteLine("Умножение невозможно");
+        return new int[rows, columns2];
+    }
+
+    // Инициализируем переменные
+    int temp = 0;
+    int[,] array_multiply = new int[rows, columns2];
+
+    // Транспонируем вторую матрицу
+    int[,] transposedArray = new int[array2.GetLength(1), array2.GetLength(0)];
+    for (int i = 0; i < array2.GetLength(1); i++)
+    {
+        for (int j = 0; j < array2.GetLength(0); j++)
+        {
+            transposedArray[i, j] = array2[j, i];
+        }
+    }
+
+    // Вычисляем произведение матриц
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns2; j++)
+        {
+            temp = 0;
+            for (int k = 0; k < columns; k++)
+            {
+                temp = temp + array[i, k] * transposedArray[j, k];
+            }
+            array_multiply[i, j] = temp;
+        }
+    }
+
+    // Выводим результат умножения на экран
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns2; j++)
+        {
+            Write($"{array_multiply[i, j]} ");
+        }
+        WriteLine();
+    }
+
+    return array_multiply;
+}
+
+// Очищаем консоль и запрашиваем у пользователя размеры матриц
+Clear();
+Write("Введите количество строк и столбцов первой матрицы: ");
+int rows = int.Parse(ReadLine());
+int columns = int.Parse(ReadLine());
+Write("Введите количество строк и столбцов второй матрицы: ");
+int rows2 = int.Parse(ReadLine());
+int columns2 = int.Parse(ReadLine());
+
+// Создаем первую матрицу и выводим ее на экран
+int[,] array = GetArray(rows, columns);
+WriteLine();
+
+// Создаем вторую матрицу и выводим ее на экран
+int[,] array2 = GetArray(rows2, columns2);
+WriteLine();
+
+// Умножаем две матрицы и выводим результат на экран
+int[,] array_multiply = MatrMultiply(array, array2);
---
+++ DZ/62/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using static System.Console;
+
+class Program
+{
+    // Функция для создания двумерного массива и заполнения его случайными числами
+    static int[,] GetArray(int rows, int columns)
+    {
+        int[,] array = new int[rows, columns];
+        Random random = new Random();
+
+        // Проходим по каждой ячейке массива и заполняем ее случайным числом в диапазоне от 10 до 99
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                array[i, j] = random.Next(10, 100);
+                Write($"{array[i, j]} ");
+            }
+            WriteLine();
+        }
+
+        return array;
+    }
+
+    // Функция для преобразования массива в спиральную форму
+    static int[,] SpiralFormating(int[,] array)
+    {
+        int rows = array.GetLength(0);
+        int cols = array.GetLength(1);
+        int[] tempArray = new int[rows * cols]; // Временный одномерный массив для сортировки
+        int[,] result = new int[rows, cols]; // Массив в спиральной форме
+        int index = 0;
+
+        // Преобразуем двумерный массив в одномерный массив
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                tempArray[index++] = array[i, j];
+            }
+        }
+
+        Array.Sort(tempArray); // Сортируем значения в одномерном массиве
+        int left = 0, right = cols - 1, top = 0, bottom = rows - 1;
+        int num = 0; // Начальное значение для заполнения матрицы в спиральной форме
+
+        // Заполняем матрицу в спиральной форме
+        while (num < rows * cols)
+        {
+            // Движение вправо
+            for (int i = left; i <= right; i++)
+            {
+                result[top, i] = tempArray[num++];
+            }
+            top++;
+
+            // Движение вниз
+            for (int i = top; i <= bottom; i++)
+            {
+                result[i, right] = tempArray[num++];
+            }
+            right--;
+
+            // Движение влево
+            for (int i = right; i >= left; i--)
+            {
+                result[bottom, i] = tempArray[num++];
+            }
+            bottom--;
+
+            // Движение вверх
+            for (int i = bottom; i >= top; i--)
+            {
+                result[i, left] = tempArray[num++];
+            }
+            left++;
+        }
+
+        WriteLine("Матрица в спиральной форме:");
+        int maxLength = (rows * cols).ToString().Length; // Определяем максимальную длину числа для форматирования вывода
+
+        // Печатаем и возвращаем новую матрицу
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Write($"{result[i, j]:D2} ".PadLeft(maxLength + 1)); // Форматированный вывод чисел с выравниванием
+            }
+            WriteLine();
+        }
+
+        return result;
+    }
+
+    static void Main()
+    {
+        Clear(); // Очищаем консоль
+        int rows = 4, cols = 4;
+        int[,] array = GetArray(rows, cols); // Создаем массив и заполняем его случайными числами
+        WriteLine(); // Печатаем пустую строку для удобочитаемости вывода
+        SpiralFormating(array); // Преобразуем массив в спиральную форму и выводим результат
+    }
+}
